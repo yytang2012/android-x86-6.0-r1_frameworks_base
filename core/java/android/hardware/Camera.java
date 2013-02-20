09f9e6c74751089c6bb4f8633718399bc9acc9e3
Chih-Wei Huang <cwhuang@linux.org.tw>
2013-02-21 01:46:17 +0800

    camera: fix NullPointerException

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -4110,6 +4110,8 @@ public class Camera {
                 return Float.parseFloat(mMap.get(key));
             } catch (NumberFormatException ex) {
                 return defaultValue;
+            } catch (NullPointerException ex) {
+                return defaultValue;
             }
         }
 
@@ -4118,6 +4120,8 @@ public class Camera {
             try {
                 return Integer.parseInt(mMap.get(key));
             } catch (NumberFormatException ex) {
+                return defaultValue;
+            } catch (NullPointerException ex) {
                 return defaultValue;
             }
         }
