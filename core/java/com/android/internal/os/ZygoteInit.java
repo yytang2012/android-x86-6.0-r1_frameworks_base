b0c90bdc1230fdddff2d77d4c7aa7eb5e586c5c6
doheon1.lee <doheon1.lee@lge.com>
2016-01-20 13:07:27 +0900

    Changes of root storage space unmounting time on Zygote Process
    Cherr Pick from https://android-review.googlesource.com/#/c/204250/
    
    Zygote process forks every child process for launch the SystemServer and other
    applications. When child process is forked, unmount storage inherited
    from Zygote process is executed before it gains its own root storage
    space.
    If Zygote have no storage spaces, unmount operations not needed to
    get relevant permission storage space.
    Thus unmount is executed only once shortly before the SystemServer is forked.
    And the child processes do not unmount its inherited root storage space.
    
    Change-Id: Ib71b065f86d9c4eb100e79f2a74a0cd5ae761b08
    Tracked-On:https://jira01.devtools.intel.com/browse/OAM-26190
    Reviewed-on: https://android.intel.com:443/485736

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -619,6 +619,9 @@ public class ZygoteInit {
             // Zygote.
             Trace.setTracingEnabled(false);
 
+            // Zygote process unmounts root storage spaces.
+            Zygote.nativeUnmountStorageOnInit();
+
             if (startSystemServer) {
                 startSystemServer(abiList, socketName);
             }
